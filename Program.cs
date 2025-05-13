06b160b69c91c04475d32d97dc07b4b5867f6ac9
Muhammad Abdullah Kiani <22MKiani@heckgrammar.co.uk>
2025-05-13 09:50:10 +0100

    8

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace sub_strings
+﻿using System.Diagnostics.Tracing;
+
+namespace sub_strings
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string words = "Mr Milton is the best";
+
+            //Console.WriteLine(words[8]); // square bracket only slice 1 character
+            //Console.WriteLine(words.Substring(13)); // if you only give one index it will continue on until the end
+            //Console.WriteLine(words.Substring(3, 18)); // how it works
+            
+            // exp in use
+            string [] firstname = {"Jane", "Bob", "Jeff", "Daisy", "Dave"};
+            string [] lastname = { "Doe", "Smith", "Stone", "Shepherd", "White"};
+            int yearstarted = 2025;
+            string username; 
+            for (int i = 0; i < firstname.Length; i++)
+            {
+                username = Convert.ToString(yearstarted).Substring(2) + firstname[i].Substring(0, 1) + lastname[i];
+                Console.WriteLine(username);
+            }
+
+            //
+           
+
+
+
+
+
         }
     }
 }
